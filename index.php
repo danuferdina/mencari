7f562bf415d13ce6948ae08b5189e796cb758d00
Danu Ferdina <danu.ferdina9@gmail.com>
2022-09-21 15:06:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="icon" href="/favicon.ico" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Mencari</title>
+
+<meta name="title" content="Mencari" />
+<meta name="description" content="Mencari" />
+
+<meta property="og:type" content="website" />
+<meta property="og:url" content="#" />
+<meta property="og:title" content="Mencari" />
+<meta property="og:description" content="Mencari" />
+<meta property="og:image" content="/thumbnail.png" />
+<meta property="og:image:width" content="512" />
+<meta property="og:image:height" content="512" />
+
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="#" />
+<meta property="twitter:title" content="Mencari" />
+<meta property="twitter:description" content="Mencari" />
+<meta property="twitter:image" content="/thumbnail.png" />
+
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&family=Roboto:wght@200;300;400;500;600;700&family=Inter:wght@200;300;400;500;600;700&family=Roboto+Mono:wght@200;300;400;500;600;700&display=swap" rel="preload" as="style" type="text/css" media="all" onload="this.rel='stylesheet'" />
+
+
+<script>
+      (function (h, o, t, j, a, r) {
+        h.hj =
+          h.hj ||
+          function () {
+            (h.hj.q = h.hj.q || []).push(arguments);
+          };
+        h._hjSettings = { hjid: 3162422, hjsv: 6 };
+        a = o.getElementsByTagName("head")[0];
+        r = o.createElement("script");
+        r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+    </script>
+<script type="module" crossorigin src="https://autobot.co.id/assets/index.4cb2aa4d.js"></script>
+<link rel="stylesheet" href="https://autobot.co.id/assets/index.7a7a374e.css">
+</head>
+<body class="home">
+<div id="app"></div>
+</body>
+</html>
